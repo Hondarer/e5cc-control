a57f40ff4f5739243408b234d9eacc81002bbdaf
Tetsuo Honda <t-honda@hondarer-soft.com>
2021-12-12 20:31:09 +0900

    CRCの計算と付与をメソッド化

--- e5cc-control/protocolTest/Program.cs
+++ e5cc-control/protocolTest/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO.Ports;
+
 class protocolTest
 {
     static int Main(string[] args)
@@ -14,17 +15,18 @@ class protocolTest
 
         serialPort.Open();
 
-        serialPort.Write(new byte[] { 0x01, 0x08, 0x00, 0x00, 0x12, 0x34, 0xed, 0x7c }, 0, 8);
+        byte[] sendDataWithCRC = CalculateAndAddCRC(new byte[] { 0x01, 0x08, 0x00, 0x00, 0x12, 0x34 });
+        serialPort.Write(sendDataWithCRC, 0, sendDataWithCRC.Length);
 
         while (serialPort.BytesToRead < 8)
         {
             Thread.Sleep(10);
         }
 
-        byte[] recieved = new byte[8];
-        serialPort.Read(recieved, 0, 8);
+        byte[] recvDataWithCRC = new byte[8];
+        serialPort.Read(recvDataWithCRC, 0, 8);
 
-        foreach (byte data in recieved)
+        foreach (byte data in recvDataWithCRC)
         {
             Console.Write($"0x{data:x2} ");
         }
@@ -33,5 +35,30 @@ class protocolTest
         serialPort.Close();
 
         return 0;
+    }
+
+    static byte[] CalculateAndAddCRC(byte[] buffer)
+    {
+        ushort crc = 0xffff;
+
+        for (int i = 0; i < buffer.Length; i++)
+        {
+            crc ^= buffer[i];
+            for (int j = 0; j < 8; j++)
+            {
+                bool isCarray = (crc & 1) == 1;
+                crc = (ushort)(crc >> 1);
+                if (isCarray)
+                {
+                    crc ^= 0xa001;
+                }
+            }
+        }
+
+        Array.Resize(ref buffer, buffer.Length + 2);
+        buffer[^2] = (byte)(crc & 0xff);
+        buffer[^1] = (byte)(crc >> 8 & 0xff);
+
+        return buffer;
     }
 }
